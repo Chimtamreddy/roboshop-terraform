fb7f250056fb1d64a0a93d86b3a5272967f1d5e9
Chimtamreddy <kr7348202@gmail.com>
2024-04-18 15:38:43 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ module "vpc" {
   source = "git::https://github.com/Chimtamreddy/tf-module-vpc.git"
   for_each = var.vpc
   cidr = each.value["cidr"]
-
+  subnets = each.value["subnets"]
+  vpc_id = each.value["vpc_id"]
 }
