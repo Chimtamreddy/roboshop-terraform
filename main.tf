25e7323014638e5f6dfe1edf6fadc8fc10dffe1e
Chimtamreddy <kr7348202@gmail.com>
2024-04-18 21:24:12 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -29,3 +29,20 @@ module "alb" {
 }
 
 
+module "docdb" {
+
+  source = "git::https://github.com/Chimtamreddy/tf-module-docdb.git"
+  for_each = var.docdb
+
+  tags = var.tags
+  env = var.env
+  subnet_ids = local.app_subnets_cidr
+  vpc_id = local.vpc_id
+  sg_ingress_cidr = each.value["sg_ingress_cidr"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  engine_version = each.value["engine_version"]
+}
+
+
