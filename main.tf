c8a0438b4a72444251ec28a59cba7d16699c7e2c
Chimtamreddy <kr7348202@gmail.com>
2024-04-18 18:51:25 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -12,6 +12,22 @@ module "vpc" {
 
 }
 
-output "vpc" {
-  value = module.vpc
-}
+# module "alb" {
+#
+#   source = "git::https://github.com/Chimtamreddy/tf-module-alb.git"
+#   for_each = var.alb
+#
+#   tags = var.tags
+#   env = var.env
+#   lb_type = each.value["lb_type"]
+#   internal = each.value["internal"]
+#   vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+#   subnets = each.value["internal"] ? local.subnet-ids :
+#   sg_port = each.value["sg_port"]
+#   sg_ingress_cidr = each.value["sg_ingress_cidr"]
+#
+#
+# }
+
+
+variable "subnets" {}
